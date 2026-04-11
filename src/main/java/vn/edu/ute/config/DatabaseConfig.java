458b5a1014a401b8aa5fa0a6a25ae2985dc73271
Okarin <lengonhuttan2005@gmail.com>
2026-04-11 22:49:53 +0700

    Update DatabaseConfig.java

--- src/main/java/vn/edu/ute/config/DatabaseConfig.java
+++ src/main/java/vn/edu/ute/config/DatabaseConfig.java
@@ -38,11 +38,11 @@ public class DatabaseConfig {
             // Use environment variables if set, otherwise fallback to the provided Render
             // PostgreSQL credentials
             // Cấu hình remote Database (Host on Render)
-            String dbHost = getEnvOrProperty("DB_HOST", "dpg-d71n9u24d50c73bs0h3g-a.oregon-postgres.render.com");
+            String dbHost = getEnvOrProperty("DB_HOST", "dpg-d7d65of7f7vs739mfgs0-a.singapore-postgres.render.com");
             String dbPort = getEnvOrProperty("DB_PORT", "5432");
-            String dbName = getEnvOrProperty("DB_NAME", "audiogear_ecommerce");
-            String dbUser = getEnvOrProperty("DB_USER", "audiogear_ecommerce_user");
-            String dbPassword = getEnvOrProperty("DB_PASSWORD", "o82tvnOhhn6TbgArdqhyiFS1YXcRTGxP");
+            String dbName = getEnvOrProperty("DB_NAME", "audiogear_ecommerce_qw9d");
+            String dbUser = getEnvOrProperty("DB_USER", "okarin2507");
+            String dbPassword = getEnvOrProperty("DB_PASSWORD", "2bhCvsUWqfePrGx0qyVf3aIhAohq5KYA");
 
 
             //  Cấu hình Database local
